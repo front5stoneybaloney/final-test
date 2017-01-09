895d2e2fc77f0d3d0a7d5f69747344ab4c0cbcad
front5stoneybaloney <front5stoneybaloney@gmail.com>
2017-01-09 21:39:57 +0200

    Final Test
    here it goes

--- task2/script.js
+++ task2/script.js
@@ -1,18 +1,34 @@
 "use strict"
 
 // задача 1
-
 var arr = [2, 5, 8, 4, 1, 12];
+arr.sort(function(a, b){return a-b});
+
+
 
 // задача 2
-
 var text = "The quick brown fox jumps over the lazy dog";
+var list = text.split(" ");
+
+var filteredList = list.filter(function(item) {
+   
+  return item.length > 4;
+});
+
+console.log("filteredList:", filteredList);
+
+
 
 // задача 3
+var pgraph = document.createElement("P");
+    var t = document.createTextNode("repetition is fun!");
+    pgraph.appendChild(t);
+    document.body.appendChild(pgraph);
 
 
 // задача 4
 
 
 // задача 5
+document.cookie.indexOf('sessionKey')== -1
 
